1ffc7964c79ff3a4bf93c12964b3a326b4230cd8
Mateus Alves <miatempo13@gmail.com>
2023-10-12 14:49:08 -0300

    Modified enemy AI with basic roaming pathfinding.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Random = UnityEngine.Random;
 
 public class EnemyAI : MonoBehaviour {
@@ -10,45 +11,25 @@ public class EnemyAI : MonoBehaviour {
 
     private State _state;
     private EnemyPathfinding _enemyPathfinding;
-    private Rigidbody2D _rb;
-    private SpriteRenderer _spriteRenderer;
     
-    private Vector2 _movement;
-    [SerializeField] private float moveSpeed = 1f;
-
     private void Awake() {
         _state = State.Roaming;
         _enemyPathfinding = GetComponent<EnemyPathfinding>();
-        _rb = GetComponent<Rigidbody2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Start() {
         StartCoroutine(RoamingRoutine());
     }
 
-    private void FixedUpdate() {
-        Move();
-        AdjustSpriteDirection();
-    }
-
     private IEnumerator RoamingRoutine() {
         while (_state == State.Roaming) {
-            Vector2 roamPosition = GetRoamingPosition();
-            _movement = roamPosition;
+            var roamPosition = GetRoamingPosition();
+            _enemyPathfinding.MoveTo(roamPosition);
             yield return new WaitForSeconds(2f);
         }
     }
 
     private Vector2 GetRoamingPosition() {
         return new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
-    }
-    
-    private void Move() {
-        _rb.MovePosition(_rb.position + _movement * (moveSpeed * Time.fixedDeltaTime));
-    }
-
-    private void AdjustSpriteDirection() {
-        _spriteRenderer.flipX = _movement.x < Mathf.Epsilon;
     }
 }
--- Assets/Scripts/EnemyPathfinding.cs
+++ Assets/Scripts/EnemyPathfinding.cs
@@ -1,12 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPathfinding : MonoBehaviour {
-    void Start() {
+    
+    private Rigidbody2D _rb;
+    private SpriteRenderer _spriteRenderer;
+    private EnemyAI _enemyAI;
         
+    private Vector2 _movement;
+    [SerializeField] private float moveSpeed = 1f;
+    
+    private void Awake() {
+        _rb = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _enemyAI = GetComponent<EnemyAI>();
     }
-    void Update() {
-        
+
+    private void FixedUpdate() {
+        Move();
+        AdjustSpriteDirection();
+    }
+
+    private void Move() {
+        _rb.MovePosition(_rb.position + _movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    public void MoveTo(Vector2 targetPosition) {
+        _movement = targetPosition;
+    }
+
+    private void AdjustSpriteDirection() {
+        _spriteRenderer.flipX = _movement.x < Mathf.Epsilon;
     }
 }
