143bf4ba52ffe578308813070ad7c2d39a03f326
Mateus Alves <miatempo13@gmail.com>
2023-10-12 21:18:27 -0300

    Added cooldown for sword swings and functionality for holding down the attack button.

--- Assets/Scripts/Player/Sword.cs
+++ Assets/Scripts/Player/Sword.cs
@@ -10,8 +10,12 @@ public class Sword : MonoBehaviour {
     [SerializeField] private GameObject slashAnimationObject;
     [SerializeField] private Transform slashAnimationSpawnPoint;
     [SerializeField] private Transform weaponCollider;
-    
-    
+
+    [SerializeField] private float swordCooldown = 0.34f;
+
+    private bool _attackButtonDown;
+    [SerializeField] private bool _isAttacking;
+
     private GameObject _slashAnimation;
     private PlayerControls _playerControls;
     private Animator _animator;
@@ -31,19 +35,40 @@ public class Sword : MonoBehaviour {
     }
 
     private void Start() {
-        _playerControls.Combat.Attack.started += _ => Attack();
+        _playerControls.Combat.Attack.started += _ => StartAttacking();
+        _playerControls.Combat.Attack.canceled += _ => StopAttacking();
     }
 
     private void Update() {
         MouseFollowWithOffset();
+        Attack();
     }
 
     private void Attack() {
-        _animator.SetTrigger("Attack");
-        weaponCollider.gameObject.SetActive(true);
-        
-        _slashAnimation = Instantiate(slashAnimationObject, slashAnimationSpawnPoint.position, quaternion.identity);
-        _slashAnimation.transform.parent = this.transform.parent;
+        if (_attackButtonDown && !_isAttacking) {
+            _isAttacking = true;
+            _animator.SetTrigger("Attack");
+            weaponCollider.gameObject.SetActive(true);
+            
+            _slashAnimation = Instantiate(slashAnimationObject, slashAnimationSpawnPoint.position, quaternion.identity);
+            _slashAnimation.transform.parent = this.transform.parent;
+            
+            StartCoroutine(AttackCooldownRoutine());
+        }
+    }
+
+    private void StartAttacking() {
+        _attackButtonDown = true;
+    }
+
+    private void StopAttacking() {
+        _attackButtonDown = false;
+    }
+
+    private IEnumerator AttackCooldownRoutine() {
+        yield return new WaitForSeconds(swordCooldown);
+        _isAttacking = false;
+
     }
 
     private void SwingUpFlipAnimationEvent() {
